a11a26ba3eaf432619bcd3b5a99329dc7551ef15
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-07-02 22:01:17 +0600

    added new book

--- src/pages/Home/Services/ServiceCard.jsx
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
+
 const ServiceCard = ({ service }) => {
-  const { title, img, price } = service;
+  const { title, img, price, _id } = service;
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure className="px-10 pt-10">
@@ -10,7 +12,9 @@ const ServiceCard = ({ service }) => {
         <p>Price: ${price}</p>
         <p>If a dog chews shoes whose shoes does he choose?</p>
         <div className="card-actions">
-          <button className="btn btn-primary">Buy Now</button>
+          <Link to={`/checkout/${_id}`}>
+            <button className="btn btn-primary">Book Now</button>
+          </Link>
         </div>
       </div>
     </div>
