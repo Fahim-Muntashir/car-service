d7f01fd8d7da8c2a46c04084b4ccade418b6ad7e
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-07-02 11:17:15 +0600

    cover slider added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,5 +1,17 @@
+import Banner from "../Banner/Banner";
+import image1 from "../../../assets/images/banner/1.jpg";
+import image2 from "../../../assets/images/banner/2.jpg";
+import image3 from "../../../assets/images/banner/3.jpg";
+import image4 from "../../../assets/images/banner/4.jpg";
+import image5 from "../../../assets/images/banner/5.jpg";
+import image6 from "../../../assets/images/banner/6.jpg";
+
 const Home = () => {
-  return <div>thos is hone</div>;
+  return (
+    <div>
+      <Banner></Banner>
+    </div>
+  );
 };
 
 export default Home;
