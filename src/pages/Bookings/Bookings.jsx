202b1a1985b64f442ecbd02243a14c1a8f60eaab
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-07-03 10:58:41 +0600

    booking delete added from state and servevr

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
+import BookingRow from "./BookingRow";
 
 const Bookings = () => {
   const user = useContext(AuthContext);
@@ -13,9 +14,55 @@ const Bookings = () => {
       .then((data) => setBookings(data));
   }, []);
 
+  const handleDelete = (id) => {
+    const proceed = confirm("are you u sure u want to delete");
+    if (proceed) {
+      fetch(`http://localhost:5000/bookings/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert("deleted successfull");
+            const remaining = bookings.filter((booking) => booking._id !== id);
+            setBookings(remaining);
+          }
+        });
+    }
+  };
+
   return (
     <div>
       <h2 className="text-5xl">Your Booking: {bookings.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Image</th>
+              <th>Service</th>
+              <th>Date</th>
+              <th>Price</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookings.map((booking) => (
+              <BookingRow
+                key={booking._id}
+                handleDelete={handleDelete}
+                booking={booking}
+              ></BookingRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
