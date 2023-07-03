9f99fa4663da2c9c1ae188a795113f0b5f11cb65
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-07-03 10:14:27 +0600

    private route added

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -5,15 +5,19 @@ const Bookings = () => {
   const user = useContext(AuthContext);
   const [bookings, setBookings] = useState([]);
 
-  const url = `http://localhost:5000/bookings?email=${user.email}`;
+  const url = `http://localhost:5000/bookings?email=${user?.email}`;
 
   useEffect(() => {
     fetch(url)
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => setBookings(data));
   }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      <h2 className="text-5xl">Your Booking: {bookings.length}</h2>
+    </div>
+  );
 };
 
 export default Bookings;
