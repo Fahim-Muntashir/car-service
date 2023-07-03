68e59abe292831f54249468c5a67eddc8ae799c9
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-07-03 11:30:06 +0600

    patch with last commit maybe another day maybe update some feature

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -12,7 +12,7 @@ const Bookings = () => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => setBookings(data));
-  }, []);
+  }, [url]);
 
   const handleDelete = (id) => {
     const proceed = confirm("are you u sure u want to delete");
@@ -30,6 +30,26 @@ const Bookings = () => {
           }
         });
     }
+  };
+
+  const handleBookingConfirm = (id) => {
+    fetch(`http://localhost:5000/bookings/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ status: "confirm" }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          const remaining = bookings.filter((booking) => booking._id !== id);
+          const updated = bookings.find((booking) => booking._id === id);
+          updated.status = "confirm";
+          const newBooking = [updated, ...remaining];
+          setBookings(newBooking);
+        }
+      });
   };
 
   return (
@@ -58,6 +78,7 @@ const Bookings = () => {
                 key={booking._id}
                 handleDelete={handleDelete}
                 booking={booking}
+                handleBookingConfirm={handleBookingConfirm}
               ></BookingRow>
             ))}
           </tbody>
